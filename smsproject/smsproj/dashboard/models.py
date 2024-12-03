fbf60d7447186cc0aa987575d1e4d6c2a2aec724
JKM28 <johnkennethmabale@gmail.com>
2024-12-03 11:32:23 +0800

    Removed sensitive information

--- smsproject/smsproj/dashboard/models.py
+++ smsproject/smsproj/dashboard/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from twilio.rest import Client
+import os
+TWILIO_API_KEY = os.getenv("TWILIO_API_KEY")
+
 
 class Message(models.Model):
     name = models.CharField(max_length=200)
